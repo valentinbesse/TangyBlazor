c52c6d9fffeb9c2b0a16822194c1f81a7b3f8b0a
Valentin Besse <valentinbesse@aumbox.net>
2022-08-05 09:12:52 +0200

    Update

--- Tangy_Business/Repository/IRepository/IProductRepository.cs
+++ Tangy_Business/Repository/IRepository/IProductRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tangy_Models
+using Tangy_Models;
 
 namespace Tangy_Business.Repository.IRepository
 {
